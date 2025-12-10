b0ff9105fe45a2bb666f0d3c5894ccb3026d6c94
Timosha_Moscow <roll3ogurec0@gmail.com>
2025-12-10 20:59:16 +0300

    Update main.js

--- main.js
+++ main.js
@@ -18,6 +18,7 @@ files.forEach(file => {
 const navLinks = document.querySelectorAll('.nav-link');
 const burger = document.querySelector('.burger');
 const navList = document.querySelector('.nav-list');
+const login = document.querySelector('.login');
 const sections = document.querySelectorAll('section');
 
 // Service Worker
